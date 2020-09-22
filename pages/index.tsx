e0b0f1b30df18fd88c6f5d75abc95e98a712e40d
maxikgreat <maximvasylenko228322@gmail.com>
2020-09-22 21:35:34 +0200

    Added base css && respo font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div>
-      Hello from famogram
+      <h1>This text must be responsive</h1>
+      <h2>This text must be responsive</h2>
+      <h3>This text must be responsive</h3>
+      <h4>This text must be responsive</h4>
+      <h5>This text must be responsive</h5>
+      <h6>This text must be responsive</h6>
+      <p>This text must be responsive</p>
+      <span>This text must be responsive</span>
     </div>
   )
 }
