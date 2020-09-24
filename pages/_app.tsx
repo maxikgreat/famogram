3e4b63f87aa16971da2da91d25313f8fed382306
maxikgreat <maximvasylenko228322@gmail.com>
2020-09-24 08:14:11 +0200

    Added progress && resposo template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from 'next/app'
+import NextNprogress from 'nextjs-progressbar';
 
 import 'semantic-ui-css/semantic.min.css';
 import '@/styles/main.scss';
 
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#eb4ecb"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        options={{ showSpinner: false }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
---
+++ pages/feed.tsx
@@ -0,0 +1,16 @@
+import { BaseLayout } from '@components/layouts';
+
+export default function Feed() {
+  return (
+    <BaseLayout>
+      <h1>This text must be responsive</h1>
+      <h2>This text must be responsive</h2>
+      <h3>This text must be responsive</h3>
+      <h4>This text must be responsive</h4>
+      <h5>This text must be responsive</h5>
+      <h6>This text must be responsive</h6>
+      <p>This text must be responsive</p>
+      <span>This text must be responsive</span>
+    </BaseLayout>
+  )
+}
