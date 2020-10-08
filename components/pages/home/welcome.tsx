cc3cf86e6cb00da50703518ebcb07c2721447847
maxikgreat <maximvasylenko228322@gmail.com>
2020-10-08 12:42:42 +0200

    Header style added

--- components/pages/home/welcome.tsx
+++ components/pages/home/welcome.tsx
@@ -1,4 +1,4 @@
-import { Container } from 'semantic-ui-react';
+import { Container, Button, Divider } from 'semantic-ui-react';
 import { motion } from 'framer-motion';
 
 export const Welcome = () => {
@@ -9,42 +9,21 @@ export const Welcome = () => {
       <div className="bg-gradient bg2"></div>
       <div className="bg0gradient bg3"></div>
       <Container className="welcome">
-        <div></div>
-        <div>
-          <h2
-            className="bg-accent skew" 
-            // animate={{ x: 20 }}
-            // transition={{ 
-            //   repeat: Infinity, 
-            //   repeatType: 'mirror', 
-            //   duration: 2,
-            //   ease: 'easeInOut',
-            //   delay: 0.33
-            // }}
-          >Lorem</h2>
-          <h2
-            className="bg-secondary skew" 
-            // animate={{ x: 20 }}
-            // transition={{ 
-            //   repeat: Infinity, 
-            //   repeatType: 'mirror', 
-            //   duration: 2,
-            //   ease: 'easeInOut',
-            //   delay: 0.66
-            // }}
-          >Ipsum</h2>
-          <h2
-            className="bg-primary skew" 
-            // animate={{ x: 20 }}
-            // transition={{ 
-            //   repeat: Infinity, 
-            //   repeatType: 'mirror', 
-            //   duration: 2,
-            //   ease: 'easeInOut',
-            //   delay: 0.99
-            // }}
-          >Vensa</h2>
-          <div className="img-container">
+        <div className="content-hld">
+          <h1 className="ddd-accent">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, recusandae?
+          </h1>
+
+          <Divider className="skew" />
+          <button 
+            className=""
+          >discover</button>
+        </div>
+        <div className="photo-hld">
+          <h2 className="bg-accent skew">Lorem</h2>
+          <h2 className="bg-secondary skew">Ipsum</h2>
+          <h2 className="bg-primary skew">Vensa</h2>
+          <div className="img-container skew round shadow">
             <img src="/assets/images/welcome1.jpg" />
           </div>
           
