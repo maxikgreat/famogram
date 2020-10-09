032304815e4628e4d300fe8f2516747487f7e97c
maxikgreat <maximvasylenko228322@gmail.com>
2020-10-09 14:39:47 +0200

    Fix welcome bg

--- vanilla/stickyHeader.ts
+++ vanilla/stickyHeader.ts
@@ -2,7 +2,7 @@ if (typeof window !== 'undefined') {
   window.addEventListener('scroll', () => {
     let header = document.querySelector('header');
     if (header) {
-      header.classList.toggle('sticky', window.scrollY > 25);
+      header.classList.toggle('sticky', window.scrollY > 10);
     }
   });
 }
