a84f1d38f91b2e27568afdd23135a5babd90ff04
maxikgreat <maximvasylenko228322@gmail.com>
2020-10-11 21:51:49 +0200

    Choose section

--- components/pages/home/choose.tsx
+++ components/pages/home/choose.tsx
@@ -5,25 +5,50 @@ import { useEffect } from 'react';
 import { observe } from '@/functions/observer';
 import { Mode } from '@/types';
 
+const mode = {
+  finder: {
+    name: 'Finder',
+    title: 'to advertise',
+    imgSrc: '/assets/images/chose-finder.jpg',
+  },
+  bloger: {
+    name: 'Bloger',
+    title: 'to be advertised',
+    imgSrc: '/assets/images/chose-finder.jpg',
+  }
+}
+
+interface CardProps {
+  name: string,
+  title: string,
+  imgSrc: string,
+}
+
+const Card = ({ name, title, imgSrc }: CardProps) => (
+  <div className="card shadow">
+    <div className="card-img-hld">
+      <img src={imgSrc} alt={name} />
+    </div>
+    <div className="card-content-hld">
+      <h1>LEFT Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, veritatis.</h1>
+    </div>
+  </div>
+);
+
 export const Choose = () => {
-  // const sectionRef = useRef()
-  // @ts-ignore
-  // const { scrollYProgress } = useElementScroll(sectionRef); 
   useEffect(() =>{
     observe('.content-hld.hidden');
   }, []);
+
+  const { finder, bloger } = mode;
 
   return (
     <section className="choose-hld parallax parallax-one">
       <Container className="choose">
         <motion.h2 className="bg-accent">I want to...</motion.h2>
-        <div className="content-hld hidden">
-          <div>
-            <h1>LEFT Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, veritatis.</h1>
-          </div>
-          <div>
-            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, totam.</h1>
-          </div>
+        <div className="content-hld hidden skew">
+          <Card name={finder.name} title={finder.title} imgSrc={finder.imgSrc} />
+          <Card name={bloger.name} title={bloger.title} imgSrc={bloger.imgSrc} />
         </div>
       </Container>
     </section>
--- functions/observer.ts
+++ functions/observer.ts
@@ -1,20 +1,11 @@
 
 
-
-
 export const observe = (elementSelector: string): void => {
   if (typeof window !== 'undefined') {
     const element = document.querySelector(elementSelector);
     if (element) {
       const observer = new IntersectionObserver((entries) => {
-        // isIntersecting is true when element and viewport are overlapping
-        // isIntersecting is false when element and viewport don't overlap
         element.classList.toggle('hidden', !entries[0].isIntersecting);
-        // if(entries[0].isIntersecting === true) {
-        //   element.classList.remove('hidden');
-        // } else {
-
-        // }
       }, { threshold: [0.4] });
       observer.observe(element);
     }
