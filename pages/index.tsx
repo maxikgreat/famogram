8ac473639251840af5093e6b906af9ed0b080a1f
maxikgreat <maximvasylenko228322@gmail.com>
2020-10-28 16:01:36 +0100

    Home section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,65 +3,20 @@ import { BaseLayout } from '@/components/layouts';
 export default function Home() {
   return (
     <BaseLayout className="home">
-      <section class="singular-banner home-banner">
-        <div class="container">
-          <div class="singular-table-wrap">
-            <div class="singular-align-wrap">
-              <div class="singular-center-wrap">
-                <h1 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">Supercharge your design talks</h1>
-                <p class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">On the other hand, we denounce with righteous indignation and dislike men who are</p>
-                <a class="btn btn-red wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s" href="#0">Try for free</a>
-                <a class="btn btn-border wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s" href="#0">See pricing</a>
-                <span class="wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">Fully featured 30-day free trial</span>
-              </div>
+      <section className="row g-0 align-items-center">
+        <div className="col-lg-6 bg-white">
+          <div className="fabrx-min-height pb-5 pt-4 pt-lg-5">
+            <div className="fabrx-caption pr-0 pr-lg-5">
+              <h4 className="font-weight-regular">Short headline</h4>
+              <h1 className="font-weight-bold mt-3 mt-lg-4 ddd-primary">Super awesome headline</h1>
+              <p className="h6">Realm of the galaxies across the centuries the carbon in our apple pies vanquish the impossible another world venture. Dream of the mind's eye muse about home.</p>
+              <button className="btn btn-primary btn-lg mt-4">Learn more</button>
             </div>
           </div>
         </div>
-      </section>
-      <section class="singular-explore-brands">
-        <div class="container">
-          <div class="singular-section-title center-style">
-            <div class="container">
-              <h2 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">We love to explore new ways to <br />engage with brands and reach</h2>
-              <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">Nor again is there anyone who loves or pursues or desires to<br /> obtain pain of itself, because it is pain, but because.</p>
-            </div>
-          </div>
-          <div class="singular-video-wrap">
-            <img class="wow bounceInUp"  data-wow-duration="1s" data-wow-delay="0.5s" src="images/video-poster.svg" alt="Video Poster" />
-            <a class="play-icon wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s" href="#0"><img src="images/icons/play-icon.svg" alt="Play" /></a>
-          </div>
-        </div>
-      </section>
-      <section class="singular-explore-brands">
-        <div class="container">
-          <div class="singular-section-title center-style">
-            <div class="container">
-              <h2 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">We love to explore new ways to <br />engage with brands and reach</h2>
-              <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">Nor again is there anyone who loves or pursues or desires to<br /> obtain pain of itself, because it is pain, but because.</p>
-            </div>
-          </div>
-          <div class="singular-video-wrap">
-            <img class="wow bounceInUp"  data-wow-duration="1s" data-wow-delay="0.5s" src="images/video-poster.svg" alt="Video Poster" />
-            <a class="play-icon wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s" href="#0"><img src="images/icons/play-icon.svg" alt="Play" /></a>
-          </div>
-        </div>
-      </section>
-      <section class="singular-brands-reach">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 col-md-12">
-              <div class="singular-section-title wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-                <span>100+ Integrations</span>
-                <h2>We love to explore new ways to engage with brands and reach</h2>
-                <p>Nor again is there anyone who loves or<br /> pursues or desires to obtain pain of itself,<br /> because it is pain, but because.</p>
-                <a href="#0">View all integrations <img src="images/icons/right-arrow.svg" alt="Go" /></a>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-12">
-              <div class="singular-image-wrap wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                <img src="images/brands-reach.svg" alt="Brands Reach" />
-              </div>
-            </div>
+        <div className="col-lg-6">
+          <div className="text-right py-5 mt-0 mt-lg-5 pl-5 overflow-hidden">
+            <img src="/assets/images/vectors/vector-36.svg" style={{marginRight: "-75px"}} alt="Vector" />
           </div>
         </div>
       </section>
