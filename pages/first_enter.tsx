df5b1f8128bf9bb7ee1c8701a25afa8be4bfa392
maxikgreat <maximvasylenko228322@gmail.com>
2020-11-09 10:19:22 +0100

    New stepper

--- pages/first_enter.tsx
+++ pages/first_enter.tsx
@@ -2,36 +2,55 @@ import { BaseLayout } from '@/components/layouts'
 
 export default function FirstEnter() {
   return (
-    <BaseLayout className="first-enter">
-      <section className="fabrx-hero mt-5">
-        <div className="container stepper-hld">
-          <div className="d-flex justify-content-between pt-4 pb-2">
-            <div className="d-flex align-items-center step-hld">
-              <div className="circle-hld">
-                <span className="h5 mb-0">1</span>
-                <svg height="100" width="100">
-                  <circle cx="50" cy="50" r="40" stroke="#f39933" stroke-width="3" fill="#212121" />
-                </svg>
+    <BaseLayout classNameName="first-enter">
+      <section className="fabrx-section bg-white mt-5">
+        <div className="container py-0 py-md-5">
+          <div className="pr-0 pt-0 pt-lg-3 pb-4 pb-md-5">
+            <ul className="nav nav-tabs nav-tabs-md bg-transparent nav-tabs-line nav-justified">
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#Active">1. Active</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#Inactive1">2. Inactive</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" data-toggle="tab" href="#Inactive2">3. Inactive</a>
+              </li>
+            </ul>
+            <div className="tab-content" id="myTabContent">
+              <div className="tab-pane fade" id="Active" role="tabpanel">
+                <div className="row hero-caption pt-4 pr-0 pr-sm-2 pr-xl-5">
+                  <p>Realm of the galaxies across the centuries the carbon in our apple pies vanquish the impossible another world venture. Dream of the mind's eye muse about the only home we've ever known the only home we've ever known concept of the number one gathered by gravity? Stirred by starlight the sky calls to us rich in mystery paroxysm of global death with pretty stories for which there's little good evidence the only home</p>
+                  <a href="#0" className="btn btn-link mt-2 mb-3 mb-md-0 d-flex justify-content-end">
+                    <span className="btn-text">Learn more</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                      <path data-name="Icon Color" d="M10.909,5.818H0V2.909H10.909V0L16,4.243,10.909,8.485Z" transform="translate(0 4)" fill="#006eff"></path>
+                    </svg>
+                  </a>
+                </div>
               </div>
-              <h2 className="h6 mb-0">Step lorem ipsum</h2>
-            </div>
-            <div className="d-flex align-items-center step-hld">
-              <div className="circle-hld">
-                <span className="h5 mb-0">2</span>
-                <svg height="100" width="100">
-                  <circle cx="50" cy="50" r="40" stroke="#f39933" stroke-width="3" fill="#212121" />
-                </svg>
+              <div className="tab-pane fade" id="Inactive1" role="tabpanel">
+                <div className="row hero-caption pt-4 pr-0 pr-sm-2 pr-xl-5">
+                  <p>Realm of the galaxies across the centuries the carbon in our apple pies vanquish the impossible another world venture. Dream of the mind's eye muse about the only home we've ever known the only home we've ever known concept of the number one gathered by gravity? Stirred by starlight the sky calls to us rich in mystery paroxysm of global death with pretty stories for which there's little good evidence the only home</p>
+                  <a href="#0" className="btn btn-link mt-2 mb-3 mb-md-0 d-flex justify-content-end">
+                    <span className="btn-text">Learn more</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                      <path data-name="Icon Color" d="M10.909,5.818H0V2.909H10.909V0L16,4.243,10.909,8.485Z" transform="translate(0 4)" fill="#006eff"></path>
+                    </svg>
+                  </a>
+                </div>
               </div>
-              <h2 className="h6 mb-0">Step lorem ipsum</h2>
-            </div>
-            <div className="d-flex align-items-center step-hld">
-              <div className="circle-hld">
-                <span className="h5 mb-0">3</span>
-                <svg height="100" width="100">
-                  <circle cx="50" cy="50" r="40" stroke="#f39933" stroke-width="3" fill="#212121" />
-                </svg>
+              <div className="tab-pane fade active show" id="Inactive2" role="tabpanel">
+                <div className="row hero-caption pt-4 pr-0 pr-sm-2 pr-xl-5">
+                  <p>Realm of the galaxies across the centuries the carbon in our apple pies vanquish the impossible another world venture. Dream of the mind's eye muse about the only home we've ever known the only home we've ever known concept of the number one gathered by gravity? Stirred by starlight the sky calls to us rich in mystery paroxysm of global death with pretty stories for which there's little good evidence the only home</p>
+                  <a href="#0" className="btn btn-link mt-2 mb-3 mb-md-0 d-flex justify-content-end">
+                    <span className="btn-text">Learn more</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                      <path data-name="Icon Color" d="M10.909,5.818H0V2.909H10.909V0L16,4.243,10.909,8.485Z" transform="translate(0 4)" fill="#006eff"></path>
+                    </svg>
+                  </a>
+                </div>
               </div>
-              <h2 className="h6 mb-0">Step lorem ipsum</h2>
             </div>
           </div>
         </div>
