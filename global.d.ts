192e87a5e535d5210ad9f13c32791f324255dcc0
Maksym Vasylenko <maximvasylenko228322@gmail.com>
2020-12-19 18:40:24 +0100

    Fixes

--- global.d.ts
+++ global.d.ts
@@ -8,8 +8,6 @@ declare global {
       AUTH0_DOMAIN: string,
       AUTH0_CLIENT_ID: string,
       AUTH0_CLIENT_SECRET: string,
-      AUTH0_REDIRECT_URI: string,
-      AUTH0_POST_LOGOUT_REDIRECT_URI: string,
       AUTH0_COOKIE_SECRET: string,
       AUTH0_AUDIENCE: string,
       API_URL: string,
