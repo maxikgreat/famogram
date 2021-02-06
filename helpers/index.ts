6dd6fb051eb7bda814e11336a604c14667286251
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-02-06 18:03:35 +0100

    Eslint
    * add and setup with plain config

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,26 +1,27 @@
-import { categories, Category } from '../types';
 import * as yup from 'yup';
+import { categories, Category } from '../types';
 
 export const isCategory = (value: string) => categories.includes(value as Category);
 
 export const isNumber = (string: string) => {
   if (!string) return false;
-  return !isNaN(Number(string));
-}
+  return !Number.isNaN(Number(string));
+};
 
 export const isEmail = (value: string) => {
   const email = yup.string().email().required();
-  return email.isValidSync(value)
-}
+  return email.isValidSync(value);
+};
 
 export const renameKeys = (obj: any) => {
   const newObj = { ...obj };
-  for (let key in newObj) {
+  // eslint-disable-next-line
+  for (const key of newObj) {
     if (key.includes('https://hativi.com/')) {
-      const newKey = key.split('https://hativi.com/')[1]
+      const newKey = key.split('https://hativi.com/')[1];
       newObj[newKey] = newObj[key];
       delete newObj[key];
     }
   }
   return newObj;
-}
+};
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,9 @@
-//Singular WOW Aimation JavaScript
+// Singular WOW Aimation JavaScript
 new WOW().init();
 
 // Toggle JavaScript
-document.querySelectorAll('.navbar-toggler').forEach(singularToggle => {
-  singularToggle.addEventListener('click', ({target}) => {
+document.querySelectorAll('.navbar-toggler').forEach((singularToggle) => {
+  singularToggle.addEventListener('click', ({ target }) => {
     target.closest('body').classList.toggle('overflow');
   });
 });
